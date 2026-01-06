339e9615f11d3ce3006f77f6efd8ec81f51eb050
thdeji <omotobaadedeji@gmail.com>
2026-01-06 21:08:18 +0100

    first official deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@
 function App() {
 
   return (
-    <div className="bg-blue-200 h-screen flex font-mono">
-      <div className="bg-blue-100 flex flex-col gap-4 self-center mx-10 px-4 rounded-lg">
-      <div className="md:bg-[url(./images/pattern-background-desktop.svg)] bg-[url(./images/pattern-background-mobile.svg)] bg-cover bg-no-repeat h-10">
+    <div className="md:bg-[url(./images/pattern-background-desktop.svg)] bg-[url(./images/pattern-background-mobile.svg)] bg-cover bg-no-repeat h-screen flex font-mono px-15  md:px-50 lg:px-100">
+      <div className="bg-Blue50 flex flex-col gap-4 self-center rounded-2xl">
+      <div className="bg-[url(./images/illustration-hero.svg)] bg-cover bg-no-repeat h-1/4">
+        <img src="./images/illustration-hero.svg" alt="" className="w-full h-full rounded-t-2xl"/>
       </div>
-      <div className="text-center">
+      <div className="text-center mx-10">
         <h1 className="text-2xl font-bold text-Blue950 mb-2">Order Summary</h1>
         <p className="text-Gray600">You can now listen to millions of songs, audiobooks and podcasts on any device anywhere you like</p>
       </div>
-      <div className="flex justify-between bg-Blue50 py-4 px-4 rounded-2xl">
+      <div className="flex justify-between mx-10 bg-Blue100/70 py-4 px-4 rounded-2xl">
+      <div className="flex gap-2">
         <img src="./images/icon-music.svg" alt="music" />
         <div className="flex flex-col gap-1"><p className="font-bold text-Blue950">Annual plan</p>
         <p className="text-Gray600 ">$59.99/year</p></div>
+        </div>
         <p className="underline self-center text-Blue700 cursor-pointer">Change</p>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col mx-10 gap-2">
         <button className="bg-Blue700 font-bold text-white p-4 rounded-lg shadow-xl shadow-black/50">proceed to Payment</button>
-        <button className="p-4 mb-4 text-Blue950">Cancel Order</button>
+        <button className="p-4 mb-4 font-bold text-Blue950">Cancel Order</button>
       </div>
       </div>
     </div>

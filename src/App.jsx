db92f0eae9ff99fdd41521be59f3676f777d897e
thdeji <omotobaadedeji@gmail.com>
2026-01-06 01:57:12 +0100

    save progress tues 6 6:02

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-blue-200 h-screen flex">
+      <div className="bg-blue-50 flex flex-col gap-4 self-center mx-10 px-4">
+      <div className="md:bg-[url(./images/pattern-background-desktop.svg)] bg-[url(./images/pattern-background-mobile.svg)] bg-cover bg-no-repeat h-10">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="text-center">
+        <h1>Order Summary</h1>
+        <p>You can now listen to millions of songs, audiobooks and podcasts on any device anywhere you like</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="flex justify-between">
+        <img src="./images/icon-music.svg" alt="music" />
+        <div className="flex flex-col gap-1"><p>Annual plan</p>
+        <p>$59.99/year</p></div>
+        <p>Change</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <button className="bg-Blue700 p-4 rounded-lg shadow-xl shadow-black/50">proceed to Payment</button>
+        <button className="p-4 mb-4">Cancel Order</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
